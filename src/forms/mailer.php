43cc9f217a3e67c2b118c19c5916986eaf09ba7f
Alex Clapperton <alih2106@icloud.com>
2016-10-27 21:03:19 +0100

    style(global): Update website portfolio items
    Update images on website portfolio items, updated title and meta descriptions

--- src/forms/mailer.php
+++ src/forms/mailer.php
@@ -2,44 +2,44 @@
 <?php
   if(isset($_POST['email'])){
 
-	$name =$_POST["name"];
-	$from =$_POST["email"];
-	$comment=$_POST["message"];
+	$name = $_POST["name"];
+	$from = $_POST["email"];
+	$comment = $_POST["message"];
 
 	// Email Receiver Address
 	$receiver="alih2106@sky.com";
 	$subject="Contact form details";
 
-	$message = '
-	<html>
-	<head>
-	<title>HTML email</title>
-	</head>
-	<body>
-	<table width="50%" border="0" align="center" cellpadding="0" cellspacing="0">
-	<tr>
-	<td colspan="2" align="center" valign="top"><img width="150" height="69" style=" margin-top: 15px; " src="http://www.alexclapperton.co.uk/images/logo.png"></td>
-	</tr>
-	<tr>
-	<td width="50%" align="right">&nbsp;</td>
-	<td align="left">&nbsp;</td>
-	</tr>
-	<tr>
-	<td align="right" valign="top" style="border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;">Name:</td>
-	<td align="left" valign="top" style="border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;">".$name."</td>
-	</tr>
-	<tr>
-	<td align="right" valign="top" style="border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;">Email:</td>
-	<td align="left" valign="top" style="border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;">".$from."</td>
-	</tr>
-	<tr>
-	<td align="right" valign="top" style="border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;">Message:</td>
-	<td align="left" valign="top" style="border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;">".nl2br($comment)."</td>
-	</tr>
-	</table>
-	</body>
-
-	';
+  $message = "
+  <html>
+  <head>
+  <title>HTML email</title>
+  </head>
+  <body>
+  <table width='50%' border='0' align='center' cellpadding='0' cellspacing='0'>
+  <tr>
+  <td colspan='2' align='center' valign='top'><img width='150' height='69' style='margin-top: 15px;' src='http://www.alexclapperton.co.uk/images/logo.png'></td>
+  </tr>
+  <tr>
+  <td width='50%' align='right'>&nbsp;</td>
+  <td align='left'>&nbsp;</td>
+  </tr>
+  <tr>
+  <td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Name:</td>
+  <td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$name."</td>
+  </tr>
+  <tr>
+  <td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Email:</td>
+  <td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$from."</td>
+  </tr>
+  <tr>
+  <td align='right' valign='top' style='border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Message:</td>
+  <td align='left' valign='top' style='border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".nl2br($comment)."</td>
+  </tr>
+  </table>
+  </body>
+  </html>
+  ";
 // Always set content-type when sending HTML email
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
@@ -58,4 +58,3 @@ $headers .= 'From: <'.$from.'>' . "\r\n";
 
 }
 ?>
-</'.$from.'>
