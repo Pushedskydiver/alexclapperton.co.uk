c13cb44ccac9bd202ca4fdf6036ffde5a2082783
Alex Clapperton <alih2106@icloud.com>
2018-01-14 22:58:26 +0000

    :sparkles: Add service worker

--- src/sw.js
+++ src/sw.js
@@ -10,23 +10,21 @@ var cacheFiles = [
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(cacheName)
-      .then(function(cache) {
-        //console.log('Opened cache');
-        return cache.addAll(cacheFiles);
-      })
+    caches.open(cacheName).then(function(cache) {
+      //console.log('Opened cache');
+      return cache.addAll(cacheFiles);
+    });
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Grab the asset from SW cache.
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+    caches.match(event.request).then(function(response) {
+      // Grab the asset from SW cache.
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
     }).catch(function() {
       // Can't access the network return an offline page from the cache
       return caches.match('/offline/');
