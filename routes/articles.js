c1a50228997f4644aea802699e3cafecc49c376a
Alex Clapperton <alih2106@icloud.com>
2018-01-14 15:07:47 +0000

    :art: Update article routes

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,8 @@
+import express from 'express'
 import expresslru from 'express-lru'
 
-const data = require('../_data/global.json');
-const articles = require('../_data/articles.json');
-const portfolio = require('../_data/portfolio.json');
+const router = express.Router();
+const articles = require('../services/articles');
 
 const cache = expresslru({
   max: 1000,
@@ -10,30 +10,37 @@ const cache = expresslru({
   skip: req => !!req.user
 });
 
-module.exports = app => {
-  app.get('/articles/', cache, (req, res) => {
-  	res.render('articles/index', {
-      title: 'Articles',
-      articles: true,
-      data: {
-        global: data,
-        articles: articles
-      }
-    });
+/* router params */
+router.param('slug', (req, res, next, slug) => {
+  articles.getArticle(slug).then(article => {
+    req.article = article.items[0];
+    next();
   });
+});
 
-  app.get('/articles/:post/', cache, (req, res) => {
-    const post = req.params.post;
-
-  	res.render(`articles/${post}`, {
-      title: post.replace(/-/g, ' '),
-      layout: 'post.hbs',
-      articles: true,
-      post: true,
-      data: {
-        global: data,
-        articles: articles
-      }
-    });
+router.use(function (req, res, next) {
+  articles.getArticles().then(articleCollection => {
+    req.articles = articleCollection.items;
+    next();
   });
-};
+});
+
+router.get('/articles/:slug', cache, (req, res, next) => {
+  res.render('article', {
+    title: req.article.fields.articleName,
+    article: req.article,
+    layout: 'post.hbs',
+    blog: true,
+    post: true
+  });
+});
+
+router.get('/articles/', cache, (req, res, next) => {
+  res.render('articles/index', {
+    title: 'Articles',
+    articles: req.articles,
+    blog: true
+  })
+});
+
+module.exports = router;
