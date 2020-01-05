89e03a4f7df3e55c0d9a2927e6bd11531d982280
Alex Clapperton <hi@alexclapperton.co.uk>
2020-01-05 14:22:27 +0000

    🐛 Fix issue preloading header image on about page

--- src/site/_data/aboutMe.js
+++ src/site/_data/aboutMe.js
@@ -4,14 +4,11 @@ module.exports = async function () {
   return await aboutService.getAbout().then(collection => {
     const data = collection.items[0].fields;
 
-    console.log(data.heroImage[0].fields.file.url);
-
-
     return {
       title: data.title,
       heroImage: {
-        mobile: data.heroImage[0].fields.file.url,
-        desktop: data.heroImage[1].fields.file.url,
+        mobile: data.heroImage[1].fields.file.url,
+        desktop: data.heroImage[0].fields.file.url,
       }
     }
   })
