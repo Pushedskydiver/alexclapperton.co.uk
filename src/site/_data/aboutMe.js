c4dd5996f594b1de3d732735838d455794feb8d6
Alex Clapperton <hi@alexclapperton.co.uk>
2020-01-06 07:38:22 +0000

    🎨 Update about me page

--- src/site/_data/aboutMe.js
+++ src/site/_data/aboutMe.js
@@ -3,9 +3,11 @@ const aboutService = require('../../../_services/about');
 module.exports = async function () {
   return await aboutService.getAbout().then(collection => {
     const data = collection.items[0].fields;
+    const contentBlocks = data.contentBlock.map(block => block.fields);
 
     return {
       title: data.title,
+      contentBlocks: contentBlocks,
       heroImage: {
         mobile: data.heroImage[1].fields.file.url,
         desktop: data.heroImage[0].fields.file.url,
