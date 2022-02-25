8d3e94638b276698daf35bec87f7189ccc1153fc
Alex Clapperton <hi@alexclapperton.co.uk>
2022-02-25 15:39:23 +0000

    🔥 Remove _services folder

--- _services/home.js
+++
@@ -1,9 +0,0 @@
-const client = require('./contentful').client
-
-function getHome(query) {
-  query = query || {};
-  query.content_type = 'home';
-  return client.getEntries(query);
-}
-
-exports.getHome = getHome;
