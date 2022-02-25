7c4c8c3030ba0e0f9706d86f8a69abbd5963655f
Alex Clapperton <hi@alexclapperton.co.uk>
2022-02-25 23:49:01 +0000

    🎨 Update service worker

--- src/utils/shortcodes/sw-styles.js
+++ src/utils/shortcodes/sw-styles.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = () => {
   const file = path.resolve(process.cwd(), 'src', 'cache-manifest.json');
   const contents = JSON.parse(fs.readFileSync(file, 'utf8'));
-  const cssPath = Object.values(contents).find(data => data.includes('css'));
+  const cssPath = Object.values(contents).find(data => data.includes('main.css?'));
 
   return `'/${cssPath}'`;
 }
