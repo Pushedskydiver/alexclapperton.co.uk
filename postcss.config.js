2a4d649055773d8cfe90ae36d30941de79653fe8
Alex Clapperton <hi@alexclapperton.co.uk>
2022-02-26 19:58:22 +0000

    ➕ Add postcss px to rem dependency

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,7 @@ module.exports = {
   plugins: [
     require('tailwindcss')('./tailwind.config.js'),
     require('autoprefixer'),
+    require('postcss-pxtorem')(plugin.pxtorem),
     require('postcss-sort-media-queries'),
     require('postcss-minify-selectors'),
     require('cssnano')(plugin.cssnano)
