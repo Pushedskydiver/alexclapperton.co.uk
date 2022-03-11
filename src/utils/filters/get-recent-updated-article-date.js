1f4a1a9930457a41f474a96c0e0afbf126798041
Alex Clapperton <hi@alexclapperton.co.uk>
2022-03-11 23:37:33 +0000

    🎨 Remove console log

--- src/utils/filters/get-recent-updated-article-date.js
+++ src/utils/filters/get-recent-updated-article-date.js
@@ -1,7 +1,5 @@
 module.exports = (articles) => {
   const dates = articles.map(article => article.sys.publishedAt);
 
-  console.log(dates, 'dates');
-
   return new Date(Math.max(...dates.map(date => new Date(date))));
 }
