0f1297eb76d9d29d076ab8cae19ed6159af93cbf
Alex Clapperton <hi@alexclapperton.co.uk>
2022-03-11 23:36:59 +0000

    🐛 Fix feed data

--- src/utils/filters/get-recent-updated-article-date.js
+++ src/utils/filters/get-recent-updated-article-date.js
@@ -1,5 +1,7 @@
 module.exports = (articles) => {
   const dates = articles.map(article => article.sys.publishedAt);
 
+  console.log(dates, 'dates');
+
   return new Date(Math.max(...dates.map(date => new Date(date))));
 }
