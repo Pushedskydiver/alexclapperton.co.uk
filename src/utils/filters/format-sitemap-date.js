e31e224e7f1e145c8ab624f42779c7ce5aebaab4
Alex Clapperton <hi@alexclapperton.co.uk>
2022-03-12 00:23:38 +0000

    📝 Fix sitemap lastmod date

--- src/utils/filters/format-sitemap-date.js
+++ src/utils/filters/format-sitemap-date.js
@@ -1,16 +1,3 @@
 module.exports = (date) => {
-  const d = new Date(date);
-  const year = d.getFullYear();
-  let month = d.getMonth() + 1;
-  let day = d.getDate();
-
-  if (month.toString().length < 2) {
-    month = `0${month}`;
-  }
-
-  if (day.toString().length < 2) {
-    day = `0${day}`;
-  }
-
-  return [day, month, year].join('-');
+  return new Date(date).toISOString();
 }
